602b68af91eba963a53012510501fe3a5f12c076
11Ion <ion.ladaniuc@gmail.com>
2024-12-04 13:40:25 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <main className="w-full min-h-screen">
+        <Routes>
+         
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
